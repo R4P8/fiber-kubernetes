f260e6d94545263b734df90c8fd33467d58df0fb
Rizqi Fathi Rafa <rizqirafa45@gmail.com>
2025-10-21 14:16:09 +0700

    add config

---
+++ config/database.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"gorm.io/gorm/logger"
+)
+
+var (
+	DB     *sql.DB
+	GormDB *gorm.DB
+)
+
+func DatabaseConnection(ctx context.Context) (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s search_path=public TimeZone=Asia/Jakarta",
+		host, port, user, password, dbname, sslmode)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("db connection error: %w", err)
+	}
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("ping failed: %w", err)
+	}
+
+	DB = db
+	log.Println(" Connected to PostgreSQL database successfully")
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: db,
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize GORM: %w", err)
+	}
+
+	sqlDB, err := gormDB.DB()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get underlying sql.DB: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(50)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	GormDB = gormDB
+	log.Println(" GORM successfully connected and initialized (PostgreSQL)")
+
+	return gormDB, nil
+}
